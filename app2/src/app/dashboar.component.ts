9f5b043c3638340ed8eead04c14c7d842c2a9b2a
mwasu <mwaseem@sequelmed.com>
2017-05-06 01:18:07 +0500

    added angular seed

---
+++ app2/src/app/dashboar.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by mwaseem on 5/5/2017.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'my-dashboard',
+    template:'<h3>My Dashboard</h3>'
+})
+
+export class DashboardComponent{
+
+}
